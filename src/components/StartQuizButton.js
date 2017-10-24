5551a2366fce8f84336aaf5ddbbabf40f52f4b7b
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-10-24 16:56:59 +0200

    to start rendering the questions

---
+++ src/components/StartQuizButton.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { View, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+
+import { Icon, Text } from 'react-native-elements';
+import { SCREENS } from '../utils/screens';
+import { primaryColor } from '../utils/colors';
+
+function StartQuizButton({ deck, questions, navigate, containerStyle, title }) {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={styles.text}>
+        {title}
+      </Text>
+      <TouchableOpacity>
+        <Icon
+          type="ionicon"
+          size={50}
+          name={Platform.OS === 'ios' ? 'ios-play' : 'md-play'}
+          color={primaryColor}
+          containerStyle={styles.iconContainer}
+          onPress={() => navigate(SCREENS.QUIZ, { deck, questions })}
+          raised
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  iconContainer: {
+    padding: 20,
+    justifyContent: 'center'
+  },
+  text: {
+    color: primaryColor,
+    textAlign: 'center'
+  }
+})
+
+StartQuizButton.defaultProps = {
+  title: 'Let\'s Start Quiz '
+};
+
+function mapStateToProps(state, ownProps) {
+  const { questions } = state;
+  return { questions: questions.filter(q => q.deck === ownProps.deck) };
+}
+export default connect(mapStateToProps)(StartQuizButton);
